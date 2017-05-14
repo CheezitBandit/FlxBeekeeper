f8ede5fa9a422b8a4c0873a8da6398e27d93d597
Jess <geiger117@live.missouristate.edu>
2017-05-13 22:28:21 -0500

    Adding instructions, images, and back button to instruction state

--- source/InstructionState.hx
+++ source/InstructionState.hx
@@ -11,6 +11,8 @@ import flixel.FlxSprite;
 class InstructionState extends FlxState
 {
 
+    var textInput:String;
+
     override public function create():Void
     {
         super.create();
@@ -23,11 +25,45 @@ class InstructionState extends FlxState
         title.screenCenter(X);
         add(title);
         
-        var text:FlxText = new FlxText(0, 125, 0, "How To Play:", 18, true);
-        text.screenCenter(X);
+        var title2:FlxText = new FlxText(0, 125, 0, "How To Play:", 18, true);
+        title2.screenCenter(X);
+        add(title2);
+
+        textInput = "You are the beekeeper, and must collect honey\nwithout getting stung.\n\nUse the arrow keys to move and press shift to run.\n\nIf you pick a flower, drop it\nto attract bees by pressing space.\n\nGood luck!";
+        var text:FlxText = new FlxText(0, 200, 0, textInput, 14, true);
         add(text);
 
+        var sprite:FlxSprite = new FlxSprite(100, 400);
+        sprite.loadGraphic(AssetPaths.beekeeper__png, false, 10, 20);
+        add(sprite);
+        var spriteText:FlxText = new FlxText(100, 450, 0, "Beekeeper", 12, false);
+        add(spriteText);
 
+        var beesprite:FlxSprite = new FlxSprite(225, 400);
+        beesprite.loadGraphic(AssetPaths.bee__png, false, 10, 20);
+        add(beesprite);
+        var beeText:FlxText = new FlxText(225, 450, 0, "Bee", 12, false);
+        add(beeText);
+
+        var honeysprite:FlxSprite = new FlxSprite(300, 400);
+        honeysprite.loadGraphic(AssetPaths.honeycomb__png, false, 10, 20);
+        add(honeysprite);
+        var honeyText:FlxText = new FlxText(300, 450, 0, "Honey", 12, false);
+        add(honeyText);
+
+        var flowersprite:FlxSprite = new FlxSprite(400, 400);
+        flowersprite.loadGraphic(AssetPaths.flower__png, false, 10, 20);
+        add(flowersprite);
+        var flowerText:FlxText = new FlxText(400, 450, 0, "Flower", 12, false);
+        add(flowerText);
+
+        var back:FlxButton = new FlxButton(500, 425, "Got it!", MenuState);
+        add(back);
+    }
+    
+    function MenuState():Void
+    {
+        FlxG.switchState(new MenuState());
     }
     
     override public function update(elapsed):Void
